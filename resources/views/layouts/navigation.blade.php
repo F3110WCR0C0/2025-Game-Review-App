65ab21b9deab88603c8a0288a62be9d3d9d917b1
Luca <N00240595@iadt.ie>
2025-10-20 14:16:16 +0100

    Day 6

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -5,7 +5,7 @@
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}">
+                    <a href="./">
                         <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
                     </a>
                 </div>
@@ -21,6 +21,7 @@
                     <x-nav-link :href="route('games.create')" :activate="request()->routeIs('games.index')">
                         {{__('Create A Game')}}
                     </x-nav-link>
+
                     <!-- the create link only appears for admin users --> 
                     @if(auth()->user()->role === 'admin')
                         <x-nav-link>
